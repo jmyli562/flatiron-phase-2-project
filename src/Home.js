664844565ee184da985dffab50f3c6978a949346
Jimmy Li <jmyli562@gmail.com>
2023-06-22 15:05:04 -0700

    finished initial skeleton of home page

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <h1>Excercise Guru</h1>
+      <p>
+        Welcome to our fitness hub, your ultimate resource for exercise
+        inspiration and guidance. Whether you're a seasoned athlete or a
+        beginner, we're here to help you achieve your health and wellness goals.
+        Discover expert tips, step-by-step instructions, and engaging articles
+        covering a variety of exercise modalities. Join our vibrant community of
+        like-minded individuals and find support, inspiration, and challenges
+        along the way. Unlock your full potential and transform your life
+        through exercise. Start your journey to a stronger, fitter, and
+        healthier you right here. Let's sweat, smile, and succeed together!
+      </p>
+      <button>Get started now!</button>
+    </div>
+  );
+}
+
+export default Home;
