7c51b1ffa707cc24ccb5fa1a804219f6b9cc0552
Jimmy Li <jmyli562@gmail.com>
2023-06-29 16:05:44 -0700

    application now is able to save a routine

--- src/Routines.js
+++ src/Routines.js
@@ -12,6 +12,90 @@ function Routines() {
     "Sunday",
   ];
 
+  function saveRoutine() {
+    const tempObj = {
+      Routines: [
+        {
+          Day: "Monday",
+          Exercises: [],
+        },
+        {
+          Day: "Tuesday",
+          Exercises: [],
+        },
+        {
+          Day: "Wednesday",
+          Exercises: [],
+        },
+        {
+          Day: "Thursday",
+          Exercises: [],
+        },
+        {
+          Day: "Friday",
+          Exercises: [],
+        },
+        {
+          Day: "Saturday",
+          Exercises: [],
+        },
+        {
+          Day: "Sunday",
+          Exercises: [],
+        },
+      ],
+    };
+
+    weekdays.forEach((weekday, index) => {
+      const routines = document.getElementsByClassName(`${weekday}`);
+      const emptyObj = {};
+      for (let child of routines[0].children) {
+        for (let exercise of child.children) {
+          if (exercise.tagName === "SELECT" && exercise.id === "exercises") {
+            emptyObj.name = exercise.value;
+          } else if (
+            exercise.tagName === "SELECT" &&
+            exercise.id === "duration"
+          ) {
+            emptyObj.duration = exercise.value;
+            tempObj.Routines[index].Exercises.push(
+              JSON.parse(JSON.stringify(emptyObj))
+            );
+          }
+        }
+      }
+    });
+
+    /*
+
+    const routines = document.getElementsByClassName("Monday");
+    const emptyObj = {};
+    for (let child of routines[0].children) {
+      for (let exercise of child.children) {
+        if (exercise.tagName === "SELECT" && exercise.id === "exercises") {
+          //console.log(exercise);
+          emptyObj.name = exercise.value;
+          //console.log(emptyObj);
+        } else if (
+          exercise.tagName === "SELECT" &&
+          exercise.id === "duration"
+        ) {
+          //console.log(exercise);
+          emptyObj.duration = exercise.value;
+          //console.log(emptyObj);
+          tempObj.Routines[0].Exercises.push(
+            JSON.parse(JSON.stringify(emptyObj))
+          );
+          //console.log(tempObj);
+        }
+      }
+    }
+
+    */
+
+    console.log(tempObj);
+  }
+
   const weekdayList = weekdays.map((weekday) => {
     return (
       <div className={weekday}>
@@ -28,6 +112,9 @@ function Routines() {
           <ul>{weekdayList}</ul>
         </div>
       </>
+      <button className="save-routine" onClick={saveRoutine}>
+        Save Routine
+      </button>
     </div>
   );
 }
